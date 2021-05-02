cfc1321c843340c3b8b4579cf929c9fb16ca307f
MinjuCo <madina.jurakholova@gmail.com>
2021-05-02 20:11:00 +0200

    Update on manifest to fix pwa and service worker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset(img/icons//apple-touch-icon.png)) }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset(img/icons/favicon-32x32.png)) }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset(img/icons/favicon-16x16.png)) }}">
         <link rel="icon" href="favicon.ico" type="image/x-icon">
-        <link rel="manifest" href="manifest.json">
+        <link rel="manifest" href="{{ asset(manifest.json) }}">
 
         <title>{{ config('app.name') ? config('app.name') : 'Scaneroo' }}</title>
 
